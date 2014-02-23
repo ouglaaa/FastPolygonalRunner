c937482e796dc55167c10854b435539a46607082
niwamaxou <niwamaxou@gmail.com>
2014-02-23 21:09:30 +0100

    plan qui bouge et boule qui se controle

---
+++ Assets/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    private MovingGround movingGround;
+    private GameObject ground;
+    private GameObject player;
+
+
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+        ground = GameObject.FindWithTag("ground");
+        player = GameObject.FindWithTag("Player");
+        movingGround = ground.GetComponent<MovingGround>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        movingGround.move();
+
+        CharacterController controller = player.GetComponent<CharacterController>();
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(-moveDirection);
+            moveDirection *= speed;
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+	}
+}
---
+++ Assets/MovingGround.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingGround : MonoBehaviour {
+
+    private float offset;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public void move () {
+        Camera.main.fieldOfView = 75;
+        offset += Time.deltaTime * 5;
+        renderer.material.mainTextureOffset = new Vector2(0, offset);
+	}
+}
